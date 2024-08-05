d0eef08465693855f8173c26d4555a36d5ecccf4
cadicallegari <cadicallegari@gmail.com>
2024-08-05 14:38:37 +0200

    Clenup deps

---
+++ deps.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+brew install fish
+brew install gcr
+brew install nvim
+brew install fzf && fzf --fish | source
+brew install visual-studio-code
+brew install bitwarden
+brew install slack
+brew install htop
