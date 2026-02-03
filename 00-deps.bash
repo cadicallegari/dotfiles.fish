51d6b2cf268c6ad7389eaae0d3172857bd90f1d8
Claudinei Callegari <cadicallegari@gmail.com>
2026-02-03 10:40:50 +0100

    Add missing dependencies

--- 00-deps.bash
+++ 00-deps.bash
@@ -5,9 +5,14 @@ brew install gcr
 brew install nvim
 brew install fzf && fzf --fish | source
 brew install visual-studio-code
+brew install antigravity
 brew install bitwarden
 brew install slack
 brew install htop
 brew install menumeters
-brew install alt-tab
 brew install raycast
+brew install lazygit
+brew install ctop
+
+brew install rust
+cargo install cgrc
