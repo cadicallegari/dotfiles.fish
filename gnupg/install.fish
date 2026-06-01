734df29df457a6186e195fd90431521c8e172ea0
Claudinei Callegari <cadicallegari@gmail.com>
2026-06-01 10:37:08 +0200

    feat(gnupg): cache passphrase for a month and fix agent reload

--- gnupg/install.fish
+++ gnupg/install.fish
@@ -5,11 +5,12 @@ test -f ~/.gnupg/gpg-agent.conf || touch ~/.gnupg/gpg-agent.conf
 # set pinentry-tty
 grep -q "pinentry-program" ~/.gnupg/gpg-agent.conf || echo "pinentry-program "(which pinentry-tty) >> ~/.gnupg/gpg-agent.conf
 
-# ask password every 24hours only
-grep -q "default-cache-ttl" ~/.gnupg/gpg-agent.conf || echo "default-cache-ttl 86400" >> ~/.gnupg/gpg-agent.conf
-grep -q "max-cache-ttl" ~/.gnupg/gpg-agent.conf || echo "max-cache-ttl 86400" >> ~/.gnupg/gpg-agent.conf
+# ask password once a month only
+set -l cache_ttl 2592000 # 30 days in seconds
+grep -q "default-cache-ttl" ~/.gnupg/gpg-agent.conf || echo "default-cache-ttl $cache_ttl" >> ~/.gnupg/gpg-agent.conf
+grep -q "max-cache-ttl" ~/.gnupg/gpg-agent.conf || echo "max-cache-ttl $cache_ttl" >> ~/.gnupg/gpg-agent.conf
 
 # reload agent
-if command -qs gpp-connect-agent
+if command -qs gpg-connect-agent
 	gpg-connect-agent reloadagent /bye
 end
